c7d443063fa6ae9ebfae3bc740fac3f7f094f1f5
Frelly0 <gisafrelly@gmail.com>
2024-03-31 13:30:25 +0200

    sec message

--- js files/script.js
+++ js files/script.js
@@ -5,10 +5,10 @@ const allSection = document.querySelector('.main-content');
 
 // document.querySelectorAll(".text").style.display = "inline-block";
 
-function pageTransitions(){
+function pageTransitions() {
     // button click active class
-    for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function(){
+    for (let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function () {
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             this.className += ' active-btn';
@@ -21,9 +21,9 @@ pageTransitions()
 
 
 // section active
-allSection.addEventListener('click', (e)=> {
+allSection.addEventListener('click', (e) => {
     const id = e.target.dataset.id;
-    if(id) {
+    if (id) {
         // remove selected from the other btns
         sectBtns.forEach((btn) => {
             btn.classList.remove('active')
@@ -38,6 +38,27 @@ allSection.addEventListener('click', (e)=> {
         const element = document.getElementById(id);
         element.classList.add('active');
     }
+})
+
+
+// ------------------ load more -----------------
+let loadMoreBtn = document.querySelector('#load');
+let currentItem = 6;
+
+loadMoreBtn.onclick = () => {
+    let boxes = [...document.querySelectorAll('.portfolios .portfolio-item')];
+    for (var i = currentItem; i < currentItem + 6; i++) {
+        boxes[i].style.display = 'inline-block';
+        // console.log(boxes[i]);
+    }
+    currentItem += 6;
+}
+
+// ---------------------btn..................
+
+const button = document.getElementById('btn');
+button.addEventListener('click', () => {
+    // alert('Message successfull send...')
 })
 
 
